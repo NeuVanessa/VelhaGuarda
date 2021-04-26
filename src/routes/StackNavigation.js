fb0a49d7ae2f53b3466c34a8e39d97223ce55747
joaorza3 <rozenaldofilho@gmail.com>
2021-04-26 11:52:17 -0300

    Criação da função quantidade

--- src/routes/StackNavigation.js
+++ src/routes/StackNavigation.js
@@ -7,6 +7,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Login from '../pages/Login';
 // Testando a rota de usuário não logado, substituir pela login pois é a primeira rota do app
 import Home from "../pages/Home";
+// Testando a rota de atendimento.
+import ServiceHelp from '../pages/ServiceHelp'
 
 
 const Stack = createStackNavigator();
@@ -22,8 +24,8 @@ export default function App() {
         /> */}
         <Stack.Screen
           options={{ headerShown: false }}
-          name="Home"
-          component={Home}
+          name="ServiceHelp"
+          component={ServiceHelp}
         />
       </Stack.Navigator>
     </NavigationContainer>
