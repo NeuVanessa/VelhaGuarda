328d4d5f4c27af89e26c3a88188c2d13207a6850
NeuVanessa <vanessashirlenee_estrela@hotmail.com>
2021-04-26 21:12:40 -0300

    register old man

---
+++ src/pages/RegisterIdoso/styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+import colors from "../../util/colors";
+import fonts from "../../util/fonts";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#03045E",
+  },
+  containerRegister: {
+    flex: 8,
+    backgroundColor: "#03045E",
+  },
+  containerClear: {},
+  containerTextInput: {
+    width: "100%",
+    marginBottom: 30,
+  },
+  inpuText: {
+    width: "97%",
+    top: 25,
+    backgroundColor: "#efefef",
+    borderRadius: 4,
+    height: 40,
+    paddingLeft: 20,
+    fontSize: 10,
+    fontFamily: "serif",
+    margin:5
+  },
+  containerButton: {
+    marginTop: 35,
+    marginBottom: 20,
+    alignItems: "center",
+    backgroundColor: "#009AED",
+    borderRadius: 4,
+    width: "100%",
+    fontFamily: "serif",
+  },
+  buttonText: {
+    fontSize: 28,
+    color: "#fff",
+  },
+});
+
+export default styles;
